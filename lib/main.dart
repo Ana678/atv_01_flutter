d162041ac71186cc0e7b55102dc00c1a3192e8c1
Ana Carolina <anacarolinaneta@hotmail.com>
2024-03-17 22:31:04 -0300

    first update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 void main() {
-  runApp(
-      MaterialApp(title: "Contador de pessoas", home: Home())); //Material App
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        systemNavigationBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.black87,
+    ));
+    runApp(
+        MaterialApp(title: "Contador de pessoas", home: Home())); //Material App
 }
 
 class Home extends StatefulWidget {
@@ -12,16 +16,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _pessoa = 0;
-  String _mensagem = "Pode entrar!";
+  String _mensagem = "Seja bem vindo(a), pode entrar!";
+  Color _bg_color_mensagem = Colors.green;
 
   void _changePeople(int delta) {
     setState(() {
       _pessoa += delta;
       if (_pessoa >= 20) {
-        _mensagem = "Lotado, não pode entrar.";
+        _mensagem = "No momento está lotado, aguarde um momento.";
+        _bg_color_mensagem = Colors.red;
         _pessoa = 20;
       } else if (_pessoa < 20 && _pessoa >= 0) {
-        _mensagem = "Pode entrar!";
+        _mensagem = "Seja bem vindo(a), pode entrar!";
+        _bg_color_mensagem = Colors.green;
       } else {
         _pessoa = 0;
       }
@@ -32,56 +39,90 @@ class _HomeState extends State<Home> {
     return Stack(
       children: <Widget>[
         Image.asset(
-          "Imagens/restaurante.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
+            "Imagens/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: double.infinity,
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Pessoas: $_pessoa",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ), //text
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
-                    ),
-                    onPressed: () {
-                      _changePeople(1);
-                    },
-                  ),
+        Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.black38,
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(32, 120, 32, 40),
+            child:  Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Column(
+                    children: <Widget>[
+                        Text(
+                            "Há $_pessoa pessoas",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 54,
+                                decoration: TextDecoration.none,
+                            ),
+                        ),
+
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(4, 8, 4, 0),
+                            child: Text(
+                                _mensagem,
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontSize: 28,
+                                    decoration: TextDecoration.none,
+                                    backgroundColor: _bg_color_mensagem.withOpacity(0.5),
+                                ),
+                                textAlign: TextAlign.center,
+                            ),
+                        ),
+                    ],
                 ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    child: Text(
-                      "-1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.cyan,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0), // Raio da borda do botão
+                            ),
+                            padding: EdgeInsets.all(16.0),
+                        ),
+                        child: Text(
+                        "Registrar Entrada",
+                        style: TextStyle(fontSize: 24.0, color: Colors.white),
+                        ),
+                        onPressed: () {
+                        _changePeople(1);
+                        },
                     ),
-                    onPressed: () {
-                      _changePeople(-1);
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              _mensagem,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30),
-            ) //text
-          ], //widget
-        ) //Column
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:Colors.deepOrange,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0), // Raio da borda do botão
+                                ),
+                                padding: EdgeInsets.all(16.0),
+                            ),
+                            child: Text(
+                            "Registrar Saída",
+                            style: TextStyle(fontSize: 24.0, color: Colors.white),
+                            ),
+                            onPressed: () {
+                            _changePeople(-1);
+                            },
+                        ),
+                    ),
+                ],
+                ),//text
+            ], //widget
+            ),//Column
+        ),
       ],
     );
   }
